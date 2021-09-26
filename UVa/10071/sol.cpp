481553447ab1efcc179912ee5cb21c0c6d8ddbec
C4t0212 <79492436+c4t0212@users.noreply.github.com>
2021-09-26 12:04:02 +0800

    Create sol.cpp

---
+++ UVa/10071/sol.cpp
@@ -0,0 +1,12 @@
+#pragma GCC optimize("Ofast")
+#define IOS ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	IOS;
+	int v, t;
+
+	while (cin >> v >> t)
+		cout << v * t * 2 << "\n";
+}
