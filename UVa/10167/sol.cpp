e31b17877e564c5867a455c48edaf2885559333b
C4t0212 <79492436+c4t0212@users.noreply.github.com>
2021-09-27 22:48:07 +0800

    Create sol.cpp

---
+++ UVa/10167/sol.cpp
@@ -0,0 +1,30 @@
+#pragma optimize("O3")
+#define IOS ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	IOS;
+	int n, cal;
+	vector<pair<int, int>> pt;
+
+	while (cin >> n && n) {
+		pt.resize(n << 1);
+
+		for (auto& i : pt) cin >> i.first >> i.second;
+
+		for (int i = -500; i <= 500; i++) for (int j = -500; j <= 500; j++) {
+			int pos = 0, neg = 0;
+
+			for (auto t : pt) {
+				cal = t.first * i + t.second * j;
+
+				if (cal > 0) pos++;
+				else if (cal < 0) neg++;
+				else neg = -1;
+			}
+
+			if (pos == neg) cout << i << " " << j << '\n', i = 9999, j = 9999;
+		}
+	}
+}
