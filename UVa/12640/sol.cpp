2928da1ae5156a27fd9f4f8470521b1aaa139536
C4t0212 <79492436+c4t0212@users.noreply.github.com>
2021-10-07 10:37:34 +0800

    Update sol.cpp

--- UVa/12640/sol.cpp
+++ UVa/12640/sol.cpp
@@ -1,10 +1,7 @@
-#pragma optimize("O3")
-#define IOS ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	IOS;
 	string s;
 	stringstream ss;
 	int ans, cur, num;
