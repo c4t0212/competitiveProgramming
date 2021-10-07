3c3f7c9fa7b53ef6b676945c571191e66089910b
C4t0212 <79492436+c4t0212@users.noreply.github.com>
2021-10-07 11:47:20 +0800

    Create sol.cpp

---
+++ UVa/10530/sol.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n, up, lw;
+	string s;
+
+	while (1) {
+		up = 11, lw = -1;
+		while (cin >> n && n) {
+			cin.get();
+			getline(cin, s);
+			if (s == "right on") {
+				if (n > lw && n < up) cout << "Stan may be honest\n";
+				else cout << "Stan is dishonest\n";
+				break;
+			}
+			else if (s == "too high") up = min(up, n);
+			else if (s == "too low") lw = max(lw, n);
+		}
+
+		if (n == 0) break;
+	}
+}
