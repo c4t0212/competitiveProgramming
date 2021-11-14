fe0c5e52ec5f904785d7bfae34049937c57e40bd
C4t0212 <79492436+c4t0212@users.noreply.github.com>
2021-11-14 23:44:54 +0800

    Update sol_2.cpp

--- UVa/10170/sol_2.cpp
+++ UVa/10170/sol_2.cpp
@@ -1,10 +1,7 @@
-#pragma GCC optimize("Ofast")
-#define IOS ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	IOS;
 	long long s, d, ans;
 
 	while(cin >> s >> d){
