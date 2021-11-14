52a6dfdfa3aee902f405578decd791f4ccadc12c
C4t0212 <79492436+c4t0212@users.noreply.github.com>
2021-11-15 00:48:41 +0800

    Update sol.cpp

--- UVa/10035/sol.cpp
+++ UVa/10035/sol.cpp
@@ -2,18 +2,14 @@
 using namespace std;
 
 int main(){
-	long long a, b, c, last, ans;
+	int a, b, last, ans;
 	
 	while(cin >> a >> b && a + b){
-		ans = 0; last = 0;
+		ans = last = 0;
 		while(a || b){
-			c = a % 10 + b % 10 + last;
+			last = a % 10 + b % 10 + (last > 9);
 			a /= 10, b /= 10;
-			if(c > 9){
-				ans++;
-				last = 1;
-			}
-			else last = 0;
+			ans += (last > 9);
 		}
 		
 		if(ans) cout << ans << (ans > 1 ? " carry operations.\n" : " carry operation.\n");
