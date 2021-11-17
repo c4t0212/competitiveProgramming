7c7d8ebecd153e6b8874634da646ee120e00ed89
C4t0212 <79492436+c4t0212@users.noreply.github.com>
2021-11-18 01:14:59 +0800

    add uva 11461-sol

--- UVa/114/11461/sol_2.cpp
+++ UVa/114/11461/sol_2.cpp
@@ -1,21 +1,19 @@
-#pragma optimize("O3")
+#pragma GCC optimize("Ofast")
 #define IOS ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
 	IOS;
-	int a, b, sqta, sqtb;
+	int a, b, ans;
 
-	while (cin >> a >> b && a) {
-		sqta = floor(sqrt(a)), sqtb = ceil(sqrt(b));
+	while(cin >> a >> b && a + b){
+		ans = 0;
 
-		if (sqta * sqta == a && sqtb * sqtb == b)
-			cout << sqtb - sqta + 1;
-		else if (sqta * sqta != a && sqtb * sqtb != b)
-			cout << sqtb - sqta - 1;
-		else
-			cout << sqtb - sqta;
-		cout << '\n';
+		for(int i = ceil(sqrt(a)); i * i <= b; i++){
+			ans++;
+		}
+
+		cout << ans << '\n';
 	}
 }
