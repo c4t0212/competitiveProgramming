1658ce3a7956fe05b5d51969146a17251e5f9037
C4t0212 <79492436+c4t0212@users.noreply.github.com>
2021-11-25 22:29:34 +0800

    Update uva12970.cpp

--- 瘋狂成設/期中考/uva12970.cpp
+++ 瘋狂成設/期中考/uva12970.cpp
@@ -6,14 +6,14 @@ int main(){
 	LL cs = 0, v1, v2, d1, d2;
 	
 	while(cin >> v1 >> d1 >> v2 >> d2 && v1 + d1 + v2 + d2){
-		printf("Case #%d: ", ++cs);
-		if(d1 * v2 < d2 * v1) printf("You owe me a beer!\n");
-		else printf("No beer for the captain.\n");
-		printf("Avg. arrival time: ");
+		cout << "Case #" << ++cs << ": ";
+		if(d1 * v2 < d2 * v1) cout << "You owe me a beer!\n";
+		else cout << "No beer for the captain.\n";
+		cout << "Avg. arrival time: ";
 		
 		LL totalD = d1 * v2 + d2 * v1, totalV = v1 * v2 * 2;
 		LL gcd = __gcd(totalD, totalV);
-		if(!(totalD % totalV)) printf("%lld\n", totalD / totalV);
-		else printf("%lld/%lld\n", totalD / gcd, totalV / gcd);
+		if(!(totalD % totalV)) cout << totalD / totalV << '\n';
+		else cout << totalD / gcd << '/' << totalV / gcd << '\n';
 	}
 }
