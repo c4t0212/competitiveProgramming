f25a5a9092746d1beea596d611f66e5ab4db54fb
C4t0212 <79492436+c4t0212@users.noreply.github.com>
2021-11-25 12:15:05 +0800

    Create 7_七折八扣.cpp

---
+++ 瘋狂成設/期中考/7_七折八扣.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int m7 = 0, m8 = 0;
+	char ipt;
+	
+	while(cin >> ipt){
+		m7 += ipt - '0';
+		m8 += ipt - '0';
+		m7 %= 7;
+		m8 %= 8;
+		m7 *= 10;
+		m8 *= 10;
+	}
+	
+	printf("%d %d\n", m7 / 10, m8 / 10);
+}
