2ff4f490c3b7b4294a89fe026981231d6d600f8a
C4t0212 <79492436+c4t0212@users.noreply.github.com>
2021-11-25 12:24:35 +0800

    Create 9_九天極數.cpp

---
+++ 瘋狂成設/期中考/9_九天極數.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int n, mx = INT_MIN, mn = INT_MAX;
+	
+	while(cin >> n){
+		mx = max(mx, n);
+		mn = min(mn, n);
+	}
+	printf("%d\n%d\n", mx, mn);
+}
